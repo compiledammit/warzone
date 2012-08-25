0a75abf4968e96e80e63c6a0d41af6aa785e9934
Joe Rinehart <joe.rinehart@gmail.com>
2012-08-25 06:25:46 -0400

    Adding bits for Grails models part 2

--- rinehart/angularTodo/grails-app/controllers/angulartodo/TodoController.groovy
+++ rinehart/angularTodo/grails-app/controllers/angulartodo/TodoController.groovy
@@ -7,10 +7,17 @@ class TodoController {
 
     static allowedMethods = [ajaxList: "GET", ajaxSave: "POST", ajaxComplete: "POST"]
 
+    /**
+     * Query for all incomplete Todos and return them as a JSON array
+     */
     def ajaxList() {
         render Todo.findAll( "from Todo t where t.complete = false order by t.dateCreated asc" ) as JSON
     }
-    
+
+    /**
+     * Get the description from the request's JSON structure, then return
+     * the current list of Todos
+     */
     def ajaxSave() {
         def todo = new Todo(
             description : request.JSON.description,
@@ -19,7 +26,10 @@ class TodoController {
 
         ajaxList()
     }
-    
+
+    /**
+     * Mark whichever Todo desired complete
+     */
     def ajaxComplete() {
         def todo = Todo.get( params.id )
         
